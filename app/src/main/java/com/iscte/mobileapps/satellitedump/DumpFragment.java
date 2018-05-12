e80828f593d85fb635e9a41219cffe4f1f450ad2
brdaa-iscteiul <brdaa@iscte-iul.pt>
2018-05-12 17:31:12 +0100

    margem dos botões adicionada

--- app/src/main/java/com/iscte/mobileapps/satellitedump/DumpFragment.java
+++ app/src/main/java/com/iscte/mobileapps/satellitedump/DumpFragment.java
@@ -35,8 +35,7 @@ public class DumpFragment extends Fragment{
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_dump, container, false);
 
